1cf64a7e86bb30c0c529c836de0e49aa4b7ce4d6
calvinbarker1023 <barkerc1@live.unc.edu>
2017-02-19 14:52:49 -0500

    Work on sensors from lecture

--- SensorsLecture/app/src/main/java/com/calvinbarker/sensorslecture/MainActivity.java
+++ SensorsLecture/app/src/main/java/com/calvinbarker/sensorslecture/MainActivity.java
@@ -7,36 +7,105 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List l = sm.getSensorList(Sensor.TYPE_ALL);
-        Sensor s = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
 
-        sm.registerListener(this, s, 1000000);
+        //List l = sm.getSensorList(Sensor.TYPE_ALL);
+        //Sensor s = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        for (int i = 0; i < l.size(); i++) {
+        //sm.registerListener(this, s, 1000000);
+
+        //Sensor s = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //sm.registerListener(this, s, 1000000);
+
+        //System.out.println("Sensor list size: " + l.size());
+
+        /*for (int i = 0; i < l.size(); i++) {
             System.out.println("Sensor: " + l.get(i).toString());
-        }
+        }*/
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        Sensor mySensor = event.sensor;
-        System.out.println(mySensor);
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 500) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    getRandomNumber();
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
 
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    private void getRandomNumber() {
+        ArrayList numbersGenerated = new ArrayList();
+
+        for (int i = 0; i < 1; i++) {
+            Random randNumber = new Random();
+            int iNumber = randNumber.nextInt(48) + 1;
+
+            if(!numbersGenerated.contains(iNumber)) {
+                numbersGenerated.add(iNumber);
+            } else {
+                i--;
+            }
+        }
+
+        TextView text = (TextView)findViewById(R.id.number_gen);
+        text.setText(""+numbersGenerated.get(0));
 
     }
 }
